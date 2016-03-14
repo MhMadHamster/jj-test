e98ebe375dacfb14ffb77f1aee13d079f928f145
Max <mhmad.hamster@gmail.com>
2016-03-14 23:54:10 +0300

    fix path

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,40 @@
 var webpack = require('webpack'),
-	precss = require('precss'),
-	cssnext = require('cssnext'),
-	clearfix = require('postcss-clearfix'),
-	prefix = require('autoprefixer'),
-	normalize = require('postcss-normalize'),
-	postcssImport = require('postcss-import');
-
-if (true) {}
+    precss = require('precss'),
+    cssnext = require('cssnext'),
+    clearfix = require('postcss-clearfix'),
+    prefix = require('autoprefixer'),
+    normalize = require('postcss-normalize'),
+    postcssImport = require('postcss-import');
 
 module.exports = {
-	entry: {
-		'bundle': './app/main.js',
-	},
-	output: {
-		path: './dist',
-		filename: '[name].js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				loader: 'babel',
-				exclude: /node_modules/,
-				query: {
-					presets: ['es2015', 'react']
-				}
-			},
-			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader?modules!postcss-loader'
-			}
-		]
-	},
-	postcss: function() {
-		return [
-			postcssImport,
-			normalize,
-			prefix,
-			precss,
-			cssnext,
-			clearfix
-		];
-	}
+    entry: './app/main.js',
+    output: {
+        filename: './dist/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?modules!postcss-loader'
+            }
+        ]
+    },
+    postcss: function() {
+        return [
+            postcssImport,
+            normalize,
+            prefix,
+            precss,
+            cssnext,
+            clearfix
+        ];
+    }
 }
